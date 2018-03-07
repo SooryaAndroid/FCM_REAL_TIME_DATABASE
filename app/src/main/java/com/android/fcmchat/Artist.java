ccef6ea766f4b4aae7c2b1a0f93a88ebbd56517d
Soorya Krishnan <soorya.k@saasvaap.com>
2018-03-07 10:56:42 +0530

    first commit

---
+++ app/src/main/java/com/android/fcmchat/Artist.java
@@ -0,0 +1,34 @@
+package com.android.fcmchat;
+
+/**
+ * Created by soorya on 5/3/18.
+ */
+
+public class Artist {
+    String artistId;
+    String artistName;
+
+    public Artist() {
+    }
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+}
